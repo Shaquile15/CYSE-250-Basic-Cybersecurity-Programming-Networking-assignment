3fdeb104f8a5a365f851c8dc221324e75d466be4
Shaquile15 <shaquilegarces@live.com>
2025-06-29 12:24:46 -0400

    square_numbers.py

--- main.py
+++
@@ -1,5 +0,0 @@
-# Duplicate lines from input to output using original line endings
-with open("input.txt", "r") as infile, open("output.txt", "w") as outfile:
-    for line in infile:
-        outfile.write(line)
-        outfile.write(line)
